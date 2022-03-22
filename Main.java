b2b8bddf06d8822de72343eaf0669cb0f2e8a6b0
167710 <101186945+167710@users.noreply.github.com>
2022-03-22 11:53:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    
+      int a = 5;
+      double b = 25;
+      char c = 'a';
+      String d="Arek";
+      System.out.println("a = " + a + "\nb =" + b + "\nc = " + c + "\nd = "+d);
+    
+  }
+}
